8e3acffcdb5a46b62054ed3a27dde285f3484da2
Stanislau Kavaliou <kavalioustanislau@gmail.com>
2016-05-14 11:12:53 +0300

    v 0.1

--- auto_tasks.py
+++ auto_tasks.py
@@ -3,7 +3,7 @@ import json
 
 from celery_runner import app
 
-from mail import send
+import mail
 
 
 @app.task()
@@ -11,11 +11,28 @@ def add():
     with open('configurations/conf1.json') as config:
         config = json.load(config)
         for conf in config['configs']:
-            s = subprocess.Popen('%s' % conf['conf'], stdout=subprocess.PIPE)
-            for line in s.stdout.readlines():
-                space_index = line.rfind(' ')
-                name = line[:space_index].strip()
-                value = int(line[space_index:].strip())
-                danger_value = conf['danger_values'].get(name)
-                if danger_value is not None and danger_value < value:
-                    send.delay(conf.get('admins')[0], 'Danger', '!!! %s %s' % (name, value))
+            process_configuration(conf)
+
+
+def process_configuration(config):
+    def parse_line(l):
+        space_index = l.rfind(' ')
+        return l[:space_index].strip(), int(l[space_index:].strip())
+
+    subprocess_result = subprocess.Popen('%s' % config['conf'], stdout=subprocess.PIPE)
+    danger, critical = {}, {}
+    for line in subprocess_result.stdout.readlines():
+        name, value = parse_line(line)
+        danger_value = config.get('danger_values', {}).get(name)
+        critical_value = config.get('critical_values', {}).get(name)
+        if critical_value is not None and critical_value < value:
+            critical[name] = value
+        elif danger_value is not None and danger_value < value:
+            danger[name] = value
+
+    # send_email_to_admins(config.get('admins', []), 'Danger', '!!! %s %s' % (name, value))
+
+
+def send_email_to_admins(admins, subject, message):
+    for admin in admins:
+        mail.send.delay(admin, subject, message)
